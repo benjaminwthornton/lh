43394c36b51a739e013afe385d19e26c27b99c85
Ryan Tucker <ryantucker0@gmail.com>
2015-03-30 22:34:45 -0400

    Landing page almost done.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var favicon = require('express-favicon');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
@@ -10,11 +11,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-app.use(favicon());
+app.use(favicon(__dirname + 'images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
