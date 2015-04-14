45dcb2fb5c53cb844cd9edcf1ddcac456b8ad609
ryantucker0 <ryantucker0@gmail.com>
2015-04-14 13:39:04 -0400

    Adding schedule page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'LyteHacks' });
 });
 
+router.get('/schedule', function(req, res) {
+  res.render('schedule');
+});
+
 module.exports = router;
