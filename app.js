06c5d381319123ca6fcc21e7ae01cfff8459f98f
Ryan Tucker <ryantucker0@gmail.com>
2015-08-24 11:14:42 -0400

    Changed some wording

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var users = require('./routes/users');
 var app = express();
 
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-app.use(favicon(__dirname + 'images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
